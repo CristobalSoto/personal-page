fe2163a422bd8d960ad252b2896402f3694140fe
Cristobal Soto <cristobalsv92@gmail.com>
2023-01-24 23:19:16 -0300

    change folder name

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
     })
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     publicPath: '/'
   },
   module: { rules },
